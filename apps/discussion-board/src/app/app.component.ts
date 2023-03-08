76fab6ace6e87c78225020a89f1c968972e33fee
Avulsed <avulsed420@gmail.com>
2023-03-08 06:09:16 +0100

    Initial commit

---
+++ apps/discussion-board/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'discussion-board-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'discussion-board';
+}
