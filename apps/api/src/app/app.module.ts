db7cf82f8c613a591bee015960c2c8e0c0e2ef75
Tizian <tizianvd@gmail.com>
2023-03-22 14:47:28 +0100

    Adding users

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { ThreadsController, ThreadsService } from '@discussion-board/threads';
+import { UserController, UserService } from '@discussion-board/user';
 
 @Module({
   imports: [],
-  controllers: [AppController, ThreadsController],
-  providers: [AppService, ThreadsService],
+  controllers: [AppController, ThreadsController, UserController],
+  providers: [AppService, ThreadsService, UserService],
 })
 export class AppModule {}
